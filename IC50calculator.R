513fdc1d1763282720e752e52639ecb64da5707d
Nelson Marreros <nelson_marreros@yahoo.fr>
2019-03-20 13:03:03 -0600

    remove unnecessary files

--- IC50calculator.R
+++
@@ -1,9 +0,0 @@
-# load necessary functions
-source("functions.R")
-
-# Check if necessary packages are installed and load them
-pack_ned <- c("shiny", "drc", "MASS", "markdown", "zip")
-install_load(pack_ned)
-
-# run the App
-runApp(launch.browser = TRUE)
